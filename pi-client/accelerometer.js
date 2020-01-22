24d166ad0bb0bedc6116e833d821f22dfa0a2e3d
G4RDS <account@g4rds.jp>
2020-01-22 17:01:33 +0900

    [fix] 正しく結果が出力されていない問題を修正

--- pi-client/accelerometer.js
+++ pi-client/accelerometer.js
@@ -85,15 +85,23 @@ class Accelerometer {
 
   // 値をひたすらとる
   while (true) {
-    const gx = acc.getGX();
-    const gy = acc.getGY();
-    const gz = acc.getGZ();
-    const ax = acc.getAX();
-    const ay = acc.getAY();
-    const az = acc.getAZ();
-    const t = acc.getTemp();
+    let gx = acc.getGX();
+    let gy = acc.getGY();
+    let gz = acc.getGZ();
+    let ax = acc.getAX();
+    let ay = acc.getAY();
+    let az = acc.getAZ();
+    let t = acc.getTemp();
 
-    await Promise.all([gx, gy, gz, ax, ay, az, t]);
+    [gx, gy, gz, ax, ay, az, t] = await Promise.all([
+      gx,
+      gy,
+      gz,
+      ax,
+      ay,
+      az,
+      t
+    ]);
 
     console.log("Gyro X", gx);
     console.log("Gyro Y", gy);
